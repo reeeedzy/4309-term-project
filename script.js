10563c6fc56b4dab37673ef080f0ed1f81f50a76
reeeedzy <reed.michael.turner@gmail.com>
2024-04-27 02:46:15 -0500

    updated whole site
    changed nav - now mobile responsive, started menu/online ordering changes, fixed format issues, etc.

--- script.js
+++ script.js
@@ -71,3 +71,107 @@ document.addEventListener("DOMContentLoaded", function () {
 	// Automatic slide change for testimonial carousel (every 8 seconds)
 	setInterval(nextTestimonialSlide, 8000);
 });
+// Navigation Interactions
+// Toggle the mobile icon to show and hide the main navigation
+// Toggle the dropdown button to show and hide the dropdown content
+document.addEventListener("DOMContentLoaded", () => {
+	const mobileToggler = document.querySelector(".navbar-toggler");
+	const navigation = document.querySelector(mobileToggler.getAttribute("data-target"));
+
+	let dropdownToggle = document.querySelectorAll(".dropdown-toggle");
+
+	mobileToggler.addEventListener("click", () => navigation.classList.toggle("collapse"));
+
+	dropdownToggle.forEach((dropdown) => {
+		dropdown.addEventListener("click", (toggler) => {
+			let dropdownElement = dropdown.parentElement;
+			dropdownElement.classList.toggle("collapse");
+		});
+	});
+});
+
+//Script for displaying background-images in the menu
+const imageArray = [
+	"images/cheese-pizza.avif",
+	"images/tour-of-italy.avif",
+	"images/chicken-parm.avif",
+	"images/fettuccini-alfredo.avif",
+	"images/spaghetti.avif",
+	"images/garlic-rolls.avif",
+	"images/mushrooms.jpg",
+	"images/calamari.avif",
+	"images/bruschetta.jpg",
+	"images/zucchini.jpg",
+	"images/fried-cheese.jpg",
+	"images/hot-wings.jpg",
+	"images/jalapeno.jpg",
+	"images/artichoke.jpg",
+	"images/garlic-bread.avif",
+	"images/salmon-salad.avif",
+	"images/greek-chicken.avif",
+	"images/house-salad.avif",
+	"images/grilled-chicken-salad.webp",
+	"images/caesar.jpeg",
+	"images/greek-salad.avif",
+	"images/shrimp.jpg",
+	"images/minestrone.jpg",
+	"images/basil.jpg",
+	"images/calzone.jpeg",
+	"images/cheese-pizza.avif",
+	"images/supreme.avif",
+	"images/margarita.avif",
+	"images/hawaiian.avif",
+	"images/prosciutto.jpg",
+	"images/focaccia.jpg",
+	"images/alfredo-pizza.jpg",
+	"images/meat-lovers.jpg",
+	"images/veggie-pizza.jpg",
+	"images/four-cheese.jpg",
+	"images/tour-of-italy.avif",
+	"images/fettuccini.avif",
+	"images/meatball-parm.avif",
+	"images/spaghetti.avif",
+	"images/baked-ziti.avif",
+	"images/meat-ravioli.avif",
+	"images/ravioli.avif",
+	"images/angel-hair-put.avif",
+	"images/pesto-penne.jpg",
+	"images/al-pana.jpg",
+	"images/tort.jpg",
+	"images/prima.jpg",
+	"images/picatta.avif",
+	"images/chicken-parm.avif",
+	"images/florintini.jpg",
+	"images/marsala.jpg",
+	"images/cacci.jpg",
+	"images/carchi.jpg",
+	"images/seafood-special.avif",
+	"images/veal-parm.avif",
+	"images/grilled-salmon.avif",
+	"images/scampi.jpg",
+	"images/diavolo.jpg",
+	"images/clams&shrimp.jpg",
+	"images/lobster-ravioli.jpg",
+	"images/tilapia.jpg",
+	"images/veal-marsala.jpg",
+	"images/veal-picatta.jpg",
+	"images/cacci.jpg",
+	"images/sausage-sub.jpg",
+	"images/meatball-sub.jpg",
+	"images/cheesesteak.jpg",
+	"images/og-burger.jpg",
+	"images/bacon-cheeseburger.jpg",
+	"images/mushroom-swiss.jpg",
+	"images/tiramisu.avif",
+	"images/chocolate-mousse.avif",
+	"images/cheesecake.webp",
+	"images/cannoli.jpeg",
+	"images/rum-cake.jpg",
+];
+
+const orderImages = document.querySelectorAll(".order-image");
+
+for (let i = 0; i < orderImages.length; i++) {
+	const imageElement = orderImages[i];
+	imageElement.style.backgroundImage = `url(${imageArray[i]})`;
+}
